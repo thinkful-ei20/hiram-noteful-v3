4024e2dd1aa206e7d2f8d8b71db2614515457711
Hiram Cruz <hiram0101@gmail.com>
2018-05-01 15:20:23 -0400

    added mongoose and models to server

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const Note = require('../models/note')
 
 /* ========== GET/READ ALL ITEM ========== */
 router.get('/', (req, res, next) => {
--- server.js
+++ server.js
@@ -1,49 +1,66 @@
-'use strict';
+"use strict"
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require(`express`)
+const morgan = require(`morgan`)
+const mongoose = require(`mongoose`)
 
-const { PORT } = require('./config');
+const { PORT, MONGODB_URI } = require(`./config`)
 
-const notesRouter = require('./routes/notes');
+const notesRouter = require(`./routes/notes`)
 
 // Create an Express application
-const app = express();
+const app = express()
 
 // Log all requests. Skip logging during
-app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
-  skip: () => process.env.NODE_ENV === 'test'
-}));
+app.use(
+  morgan(process.env.NODE_ENV === `development` ? `dev` : `common`, {
+    skip: () => process.env.NODE_ENV === `test`
+  })
+)
 
 // Create a static webserver
-app.use(express.static('public'));
+app.use(express.static(`public`))
 
 // Parse request body
-app.use(express.json());
+app.use(express.json())
 
 // Mount routers
-app.use('/api/notes', notesRouter);
+app.use(`/api/notes`, notesRouter)
 
 // Catch-all 404
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function(req, res, next) {
+  const err = new Error(`Not Found`)
+  err.status = 404
+  next(err)
+})
 
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
   res.json({
     message: err.message,
-    error: app.get('env') === 'development' ? err : {}
-  });
-});
+    error: app.get(`env`) === `development` ? err : {}
+  })
+})
 
-// Listen for incoming connections
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+if (require.main === module) {
+  mongoose.connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0]
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`)
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`)
+      console.error(`\n === Didi you remember to start 'mongod'? === \n`)
+      console.error(err)
+    })
+  // Listen for incoming connections
+  app
+    .listen(PORT, function() {
+      console.info(`Server listening on ${this.address().port}`)
+    })
+    .on(`error`, err => {
+      console.error(err)
+    })
+}
