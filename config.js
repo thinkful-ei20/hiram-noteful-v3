ef894046ff1be5c32f3089012e6b248e53da7818
Hiram Cruz <hiram0101@gmail.com>
2018-05-02 10:53:51 -0400

    added test db to config

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict"
 
-exports.PORT = process.env.PORT || 8080;
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful'
+exports.PORT = process.env.PORT || 8080
+exports.MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/noteful`
+exports.TEST_DATABASE_URI =
+  process.env.TEST_DATABASE_URI || `mongodb://localhost/noteful-test`
